4507d10ae77cba48c87e9733ce57b0a020916243
John Taylor <jaydeetay@users.noreply.github.com>
2016-03-08 10:44:49 -0500

    Port across new constellation names from 1.8 and make constellation proto canonical.

--- tools/generate.sh
+++ tools/generate.sh
@@ -6,8 +6,6 @@ libs/protobuf-java-2.6.1.jar\
 
 DATA_DIR=data
 
-java -cp $CLASSPATH \
-com.google.android.stardroid.data.ConstellationProtoWriter $DATA_DIR/constellation_names_and_lines.kml $DATA_DIR/constellations
 
 java -cp $CLASSPATH \
 com.google.android.stardroid.data.MessierProtoWriter $DATA_DIR/messier.csv $DATA_DIR/messier
