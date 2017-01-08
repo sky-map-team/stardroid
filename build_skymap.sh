8f00e17e447873fb70d717f1f7100180b6bd6459
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2017-01-08 18:58:00 +0100

    Cleanup build script
    - Add -e to error on build failures.
    - Use subshell instead of cd ..
    - Manually fix classpath because gradle has problems with flavors

--- build_skymap.sh
+++ build_skymap.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
+#!/bin/sh -e
 # Temporary script to regenerate the data and build the app.
 # TODO(jontayler): retire it once gradle can do this or
 # or we get rid of the data generation step.
 ./gradlew clean assemble installDist
-cd tools
+(cd tools
 # Gah. Gradle gets the classpath for the tools wrong.  Fix:
-sed -i -e 's/app-release.apk//g' build/install/datagen/bin/datagen
+sed -i -e 's#CLASSPATH=#CLASSPATH=$APP_HOME/lib/:#g' build/install/datagen/bin/datagen
 ./generate.sh
 ./rewrite.sh
-./binary.sh
-cd ..
+./binary.sh)
 ./gradlew assemble
-
