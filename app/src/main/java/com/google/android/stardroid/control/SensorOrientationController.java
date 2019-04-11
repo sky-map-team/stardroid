c60287d80bb2682adec9a41f6e0256f1b39f80a5
aayush singla <aayushsingla1729@gmail.com>
2019-04-11 06:37:19 +0530

    Fix #297: Abnormal App behaviour when Gyroscope is disabled

--- app/src/main/java/com/google/android/stardroid/control/SensorOrientationController.java
+++ app/src/main/java/com/google/android/stardroid/control/SensorOrientationController.java
@@ -18,17 +18,14 @@ import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.hardware.SensorListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.util.Log;
 
 import com.google.android.stardroid.ApplicationConstants;
 import com.google.android.stardroid.util.MiscUtil;
-import com.google.android.stardroid.util.smoothers.ExponentiallyWeightedSmoother;
-import com.google.android.stardroid.util.smoothers.PlainSmootherModelAdaptor;
 
 import javax.inject.Inject;
-import javax.inject.Provider;
 
 /**
  * Sets the direction of view from the orientation sensors.
@@ -36,100 +33,34 @@ import javax.inject.Provider;
  * @author John Taylor
  */
 public class SensorOrientationController extends AbstractController
-    implements SensorEventListener {
-  // TODO(johntaylor): this class needs to be refactored to use the new
-  // sensor API and to behave properly when sensors are not available.
+        implements SensorEventListener {
 
-  private static class SensorDampingSettings {
-    public float damping;
-    public int exponent;
-    public SensorDampingSettings(float damping, int exponent) {
-      this.damping = damping;
-      this.exponent = exponent;
-    }
-  }
   private final static String TAG = MiscUtil.getTag(SensorOrientationController.class);
-  /**
-   * Parameters that control the smoothing of the accelerometer and
-   * magnetic sensors.
-   */
-  private static final SensorDampingSettings[] ACC_DAMPING_SETTINGS = new SensorDampingSettings[] {
-        new SensorDampingSettings(0.7f, 3),
-        new SensorDampingSettings(0.7f, 3),
-        new SensorDampingSettings(0.1f, 3),
-        new SensorDampingSettings(0.1f, 3),
-  };
-  private static final SensorDampingSettings[] MAG_DAMPING_SETTINGS = new SensorDampingSettings[] {
-      new SensorDampingSettings(0.05f, 3),  // Derived for the Nexus One
-      new SensorDampingSettings(0.001f, 4),  // Derived for the unpatched MyTouch Slide
-      new SensorDampingSettings(0.0001f, 5),  // Just guessed for Nexus 6
-      new SensorDampingSettings(0.000001f, 5)  // Just guessed for Nexus 6
-  };
-
   private SensorManager manager;
-  private SensorListener accelerometerSmoother;
-  private SensorListener compassSmoother;
-  private Provider<PlainSmootherModelAdaptor> modelAdaptorProvider;
   private Sensor rotationSensor;
+    private Sensor geomagneticRotationSensor;
   private SharedPreferences sharedPreferences;
 
   @Inject
-  SensorOrientationController(Provider<PlainSmootherModelAdaptor> modelAdaptorProvider,
-                              SensorManager manager, SharedPreferences sharedPreferences) {
+  SensorOrientationController(SensorManager manager, SharedPreferences sharedPreferences) {
     this.manager = manager;
-    this.modelAdaptorProvider = modelAdaptorProvider;
     this.rotationSensor = manager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+          this.geomagneticRotationSensor = manager.getDefaultSensor(Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR);
+      }
     this.sharedPreferences = sharedPreferences;
   }
 
   @Override
   public void start() {
-    PlainSmootherModelAdaptor modelAdaptor = modelAdaptorProvider.get();
-
     if (manager != null) {
       if (!sharedPreferences.getBoolean(ApplicationConstants.SHARED_PREFERENCE_DISABLE_GYRO,
-          false)) {
+              false)) {
         Log.d(TAG, "Using rotation sensor");
         manager.registerListener(this, rotationSensor, SensorManager.SENSOR_DELAY_GAME);
-      } else {
-        // TODO(jontayler): remove this code once enough it's used in few enough phones.
-        Log.d(TAG, "Using classic sensors");
-        Log.d(TAG, "Exponentially weighted smoothers used");
-        String dampingPreference = sharedPreferences.getString(
-            ApplicationConstants.SENSOR_DAMPING_PREF_KEY,
-            ApplicationConstants.SENSOR_DAMPING_STANDARD);
-        String speedPreference = sharedPreferences.getString(ApplicationConstants.SENSOR_SPEED_PREF_KEY,
-            ApplicationConstants.SENSOR_SPEED_STANDARD);
-        Log.d(TAG, "Sensor damping preference " + dampingPreference);
-        Log.d(TAG, "Sensor speed preference " + speedPreference);
-        int dampingIndex = 0;
-        if (ApplicationConstants.SENSOR_DAMPING_HIGH.equals(dampingPreference)) {
-          dampingIndex = 1;
-        } else if (ApplicationConstants.SENSOR_DAMPING_EXTRA_HIGH.equals(dampingPreference)) {
-          dampingIndex = 2;
-        } else if (ApplicationConstants.SENSOR_DAMPING_REALLY_HIGH.equals(dampingPreference)) {
-          dampingIndex = 3;
-        }
-        int sensorSpeed = SensorManager.SENSOR_DELAY_GAME;
-        if (ApplicationConstants.SENSOR_SPEED_SLOW.equals(speedPreference)) {
-          sensorSpeed = SensorManager.SENSOR_DELAY_NORMAL;
-        } else if (ApplicationConstants.SENSOR_SPEED_HIGH.equals(speedPreference)) {
-          sensorSpeed = SensorManager.SENSOR_DELAY_FASTEST;
-        }
-        accelerometerSmoother = new ExponentiallyWeightedSmoother(
-            modelAdaptor,
-            ACC_DAMPING_SETTINGS[dampingIndex].damping,
-            ACC_DAMPING_SETTINGS[dampingIndex].exponent);
-        compassSmoother = new ExponentiallyWeightedSmoother(
-            modelAdaptor,
-            MAG_DAMPING_SETTINGS[dampingIndex].damping,
-            MAG_DAMPING_SETTINGS[dampingIndex].exponent);
-        manager.registerListener(accelerometerSmoother,
-                                 SensorManager.SENSOR_ACCELEROMETER,
-                                 sensorSpeed);
-        manager.registerListener(compassSmoother,
-                                 SensorManager.SENSOR_MAGNETIC_FIELD,
-                                 sensorSpeed);
+      } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+          Log.d(TAG, "Using geomagnetic_rotation sensor");
+          manager.registerListener(this, geomagneticRotationSensor, SensorManager.SENSOR_DELAY_FASTEST);
       }
     }
     Log.d(TAG, "Registered sensor listener");
@@ -137,20 +68,18 @@ public class SensorOrientationController extends AbstractController
 
   @Override
   public void stop() {
-    Log.d(
-        TAG, "Unregistering sensor listeners: " + accelerometerSmoother + ", "
-            + compassSmoother + ", " + this);
-    manager.unregisterListener(accelerometerSmoother);
-    manager.unregisterListener(compassSmoother);
     manager.unregisterListener(this);
   }
 
   @Override
   public void onSensorChanged(SensorEvent event) {
-    if (event.sensor != rotationSensor) {
-      return;
-    }
-    model.setPhoneSensorValues(event.values);
+      if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+          model.setPhoneSensorValues(event.values);
+      } else if (event.sensor.getType() == Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR) {
+          model.setPhoneSensorValues(event.values);
+      } else {
+          Log.e(TAG, "Unknown Sensor readings");
+      }
   }
 
   @Override
