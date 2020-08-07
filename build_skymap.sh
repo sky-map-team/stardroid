542b5069d5e06f69f70418fda29ecf82ec3b45e4
jontayler <jontayler@gmail.com>
2020-08-06 22:25:04 -0400

    Temporarily delete strings.xml as it's broken the build. TODO: talk to Aled about what should be in this file.

--- build_skymap.sh
+++ build_skymap.sh
@@ -2,7 +2,7 @@
 # Temporary script to regenerate the data and build the app.
 # TODO(jontayler): retire it once gradle can do this or
 # or we get rid of the data generation step.
-./gradlew clean assembleRelease installDist
+./gradlew clean assembleGms installDist
 (cd tools
 # Gah. Gradle gets the classpath for the tools wrong.  Fix:
 sed -i -e 's#CLASSPATH=#CLASSPATH=$APP_HOME/lib/:#g' build/install/datagen/bin/datagen
