bde8c3fd599823142fae8b9759368d6b470222d1
jontayler <jontayler@gmail.com>
2020-08-08 12:57:29 -0400

    SO...I did need that installDist target after all. Otherwise the datafiles don't get regenerated.

--- build_skymap.sh
+++ build_skymap.sh
@@ -2,9 +2,9 @@
 # Temporary script to regenerate the data and build the app.
 # TODO(jontayler): retire it once gradle can do this or
 # or we get rid of the data generation step.
-./gradlew clean assembleGms
+./gradlew clean assembleGms installDist
 (cd tools
-# Gah. Gradle gets the classpath for the tools wrong.  Fix:
+# Gah. Gradle gets the classpath for the tools wrong.  Fix:	# Gah. Gradle gets the classpath for the tools wrong.  Fix:
 sed -i -e 's#CLASSPATH=#CLASSPATH=$APP_HOME/lib/:#g' build/install/datagen/bin/datagen
 ./generate.sh
 ./rewrite.sh
