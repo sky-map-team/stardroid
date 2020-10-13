f2520ddb25d962949e296782087947e9e9724e2d
jontayler <jontayler@gmail.com>
2020-10-12 21:24:24 -0400

    Simplified the build script to make it faster.

--- build_skymap.sh
+++ build_skymap.sh
@@ -2,10 +2,10 @@
 # Temporary script to regenerate the data and build the app.
 # TODO(jontayler): retire it once gradle can do this or
 # or we get rid of the data generation step.
-./gradlew clean assembleGms installDist
+./gradlew clean :tools:installDist
 (cd tools
 # Gah. Gradle gets the classpath for the tools wrong.  Fix:	# Gah. Gradle gets the classpath for the tools wrong.  Fix:
 sed -i -e 's#CLASSPATH=#CLASSPATH=$APP_HOME/lib/:#g' build/install/datagen/bin/datagen
 ./generate.sh
 ./binary.sh)
-./gradlew assembleGms
+./gradlew :app:assembleGms
