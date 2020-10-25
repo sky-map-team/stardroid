e83af26b0593de99bdfac54bafd0ef6ee3a574c5
jontayler <jontayler@gmail.com>
2020-10-25 10:03:27 -0400

    OK, do this in an IDE this time so I'm less likely to screw it up.

--- app/src/androidTest/java/com/google/android/stardroid/test/SplashScreenActivityTest.java
+++ app/src/androidTest/java/com/google/android/stardroid/test/SplashScreenActivityTest.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
