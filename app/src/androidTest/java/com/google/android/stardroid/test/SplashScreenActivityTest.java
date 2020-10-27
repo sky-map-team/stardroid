a9a8d5a0cd128a3e6b46d789c23dfb130ad0d553
jontayler <jontayler@gmail.com>
2020-10-27 11:33:37 -0400

    Restore the test.

--- app/src/androidTest/java/com/google/android/stardroid/test/SplashScreenActivityTest.java
+++ app/src/androidTest/java/com/google/android/stardroid/test/SplashScreenActivityTest.java
@@ -4,7 +4,6 @@ import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -12,7 +11,6 @@ import static org.junit.Assert.assertEquals;
 public class SplashScreenActivityTest {
     @Test
     public void useAppContext() {
-	Assert.fail("I did this on purpose");
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.google.android.stardroid", appContext.getPackageName());
