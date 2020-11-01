00306efec39c3168dbfdc30d4c4a71c43ee961fb
jontayler <jontayler@gmail.com>
2020-11-01 16:59:12 -0500

    Missing import.

--- app/src/androidTest/java/com/google/android/stardroid/test/DynamicStarMapActivityTest.java
+++ app/src/androidTest/java/com/google/android/stardroid/test/DynamicStarMapActivityTest.java
@@ -20,6 +20,7 @@ import org.junit.Test;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
