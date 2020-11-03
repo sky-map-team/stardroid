4c8adcef0332f0b9d3ee13919599aaf5eaa71219
jontayler <jontayler@gmail.com>
2020-11-03 00:09:56 -0500

    Truth!

--- app/src/test/java/com/google/android/stardroid/units/LatLongRoboTest.java
+++ app/src/test/java/com/google/android/stardroid/units/LatLongRoboTest.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import static junit.framework.Assert.assertEquals;
+import static com.google.common.truth.Truth.assertThat;
 
 /**
  * Tests that require roboelectric for API calls.
@@ -14,18 +14,20 @@ import static junit.framework.Assert.assertEquals;
 @Config(manifest=Config.NONE)
 public class LatLongRoboTest {
 
+    private static final float TOL = 1e-5f;
+
     @Test
     public void latLong_testDistance() {
         LatLong point1 = new LatLong(0, 0);
         LatLong point2 = new LatLong(90, 0);
-        assertEquals(90f, point1.distanceFrom(point2));
+        assertThat(point1.distanceFrom(point2)).isWithin(TOL).of(90f);
     }
 
     @Test
     public void latLong_testDistance2() {
         LatLong point1 = new LatLong(45, 45);
         LatLong point2 = new LatLong(90, 0);
-        assertEquals(45f, point1.distanceFrom(point2), 0.05);
+        assertThat(point1.distanceFrom(point2)).isWithin(TOL).of(45f);
     }
 
 }
