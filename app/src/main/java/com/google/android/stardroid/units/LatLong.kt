3c8d157ec305da81d5e79cd9d33e4fc8d9dfe6a5
jontayler <jontayler@gmail.com>
2021-10-16 20:06:06 +0100

    Convert LatLong to Kotlin and tody up the tests.

---
+++ app/src/main/java/com/google/android/stardroid/units/LatLong.kt
@@ -0,0 +1,58 @@
+// Copyright 2008 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.android.stardroid.units
+
+import com.google.android.stardroid.util.Geometry
+import com.google.android.stardroid.util.MathUtil
+
+fun flooredMod(a: Float, n: Float): Float {
+    return (if (a < 0) a % n + n else a) % n
+}
+
+/**
+ * A simple struct for latitude and longitude.
+ */
+data class LatLong(private val _latitudeDeg: Float, private val _longitudeDeg: Float) {
+    val latitude = _latitudeDeg.coerceIn(-90f, 90f)
+    val longitude = flooredMod(_longitudeDeg + 180f, 360f) - 180f
+
+    /**
+     * This constructor automatically downcasts the latitude and longitude to
+     * floats, so that the previous constructor can be used. It is added as a
+     * convenience method, since many of the GPS methods return doubles.
+     */
+    constructor(latitude: Double, longitude: Double) : this(
+        latitude.toFloat(),
+        longitude.toFloat()
+    )
+
+    /**
+     * Angular distance between the two points.
+     * @param other
+     * @return degrees
+     */
+    fun distanceFrom(other: LatLong): Float {
+        // Some misuse of the astronomy math classes
+        val otherPnt = GeocentricCoordinates.getInstance(
+            other.longitude,
+            other.latitude
+        )
+        val thisPnt = GeocentricCoordinates.getInstance(
+            longitude,
+            latitude
+        )
+        val cosTheta = Geometry.cosineSimilarity(thisPnt, otherPnt)
+        return MathUtil.acos(cosTheta) * 180f / MathUtil.PI
+    }
+}
--- app/src/test/java/com/google/android/stardroid/units/LatLongRoboTest.kt
+++ app/src/test/java/com/google/android/stardroid/units/LatLongRoboTest.kt
@@ -9,9 +9,12 @@ import org.robolectric.annotation.Config
 /**
  * Tests that require roboelectric for API calls.
  */
+// TODO(jontayler): what was the point of this test exactly?
 @RunWith(RobolectricTestRunner::class)
 @Config(manifest = Config.NONE)
 class LatLongRoboTest {
+    private val TOL = 1e-5f
+
     @Test
     fun latLong_testDistance() {
         val point1 = LatLong(0.0, 0.0)
@@ -24,9 +27,5 @@ class LatLongRoboTest {
         val point1 = LatLong(45.0, 45.0)
         val point2 = LatLong(90.0, 0.0)
         assertThat(point1.distanceFrom(point2)).isWithin(TOL).of(45f)
-    }
-
-    companion object {
-        private const val TOL = 1e-5f
     }
 }
