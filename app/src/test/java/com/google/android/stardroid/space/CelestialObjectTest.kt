9cecce0359090e55f1a610e0b0c7370dc1fd383a
jontayler <jontayler@gmail.com>
2021-11-01 00:25:12 -0400

    Cleaned up these Truth tests.

--- app/src/test/java/com/google/android/stardroid/space/CelestialObjectTest.kt
+++ app/src/test/java/com/google/android/stardroid/space/CelestialObjectTest.kt
@@ -11,13 +11,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package com.google.android.stardroid.ephemeris
+package com.google.android.stardroid.space
 
 import com.google.android.stardroid.ephemeris.Planet
 import com.google.android.stardroid.math.LatLong
-import com.google.android.stardroid.space.CelestialObject
-import com.google.android.stardroid.space.Universe
-import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -26,7 +24,7 @@ import java.util.*
 
 @RunWith(RobolectricTestRunner::class)
 @Config(manifest = Config.NONE)
-class PlanetTest {
+class CelestialObjectTest {
     @Test
     fun testCalcNextRiseSetTime() {
         val universe = Universe()
@@ -40,49 +38,42 @@ class PlanetTest {
         var sunset = universe.solarSystemObjectFor(Planet.Sun)
             .calcNextRiseSetTime(testCal, loc, CelestialObject.RiseSetIndicator.SET)
         sunset!!.timeZone = TimeZone.getTimeZone("GMT")
-        Truth.assertThat(
-            17 <= sunset[Calendar.HOUR_OF_DAY] && 18 >= sunset[Calendar.HOUR_OF_DAY]
-        ).isTrue()
-        Truth.assertThat(sunset[Calendar.DAY_OF_MONTH]).isEqualTo(21)
+        assertThat(sunset[Calendar.HOUR_OF_DAY]).isIn(17..18)
+        assertThat(sunset[Calendar.DAY_OF_MONTH]).isEqualTo(21)
         // Sunrise should be between 5:00 and 6:59 the following day.
         var sunrise = universe.solarSystemObjectFor(Planet.Sun)
             .calcNextRiseSetTime(testCal, loc, CelestialObject.RiseSetIndicator.RISE)
         sunrise!!.timeZone = TimeZone.getTimeZone("GMT")
-        Truth.assertThat(5 <= sunrise[Calendar.HOUR_OF_DAY] && 6 >= sunrise[Calendar.HOUR_OF_DAY])
-            .isTrue()
-        Truth.assertThat(sunrise[Calendar.DAY_OF_MONTH]).isEqualTo(22)
+        assertThat(sunrise[Calendar.HOUR_OF_DAY]).isIn(5..6)
+        assertThat(sunrise[Calendar.DAY_OF_MONTH]).isEqualTo(22)
 
         // In midsummer, sunset should be between 20:00 and 21:59.
         testCal[2010, GregorianCalendar.JUNE, 21, 12, 0] = 0
         sunset = universe.solarSystemObjectFor(Planet.Sun)
             .calcNextRiseSetTime(testCal, loc, CelestialObject.RiseSetIndicator.SET)
         sunset!!.timeZone = TimeZone.getTimeZone("GMT")
-        Truth.assertThat(20 <= sunset[Calendar.HOUR_OF_DAY] && 21 >= sunset[Calendar.HOUR_OF_DAY])
-            .isTrue()
-        Truth.assertThat(sunset[Calendar.DAY_OF_MONTH]).isEqualTo(21)
+        assertThat(sunset[Calendar.HOUR_OF_DAY]).isIn(20..21)
+        assertThat(sunset[Calendar.DAY_OF_MONTH]).isEqualTo(21)
         // Sunrise should be between 2:00 and 3:59 the following day.
         sunrise = universe.solarSystemObjectFor(Planet.Sun)
             .calcNextRiseSetTime(testCal, loc, CelestialObject.RiseSetIndicator.RISE)
         sunrise!!.timeZone = TimeZone.getTimeZone("GMT")
-        Truth.assertThat(2 <= sunrise[Calendar.HOUR_OF_DAY] && 3 >= sunrise[Calendar.HOUR_OF_DAY])
-            .isTrue()
-        Truth.assertThat(sunrise[Calendar.DAY_OF_MONTH]).isEqualTo(22)
+        assertThat(sunrise[Calendar.HOUR_OF_DAY]).isIn(2..3)
+        assertThat(sunrise[Calendar.DAY_OF_MONTH]).isEqualTo(22)
 
         // In midwinter, sunset should be between 14:00 and 15:59.
         testCal[2010, GregorianCalendar.DECEMBER, 21, 12, 0] = 0
         sunset = universe.solarSystemObjectFor(Planet.Sun)
             .calcNextRiseSetTime(testCal, loc, CelestialObject.RiseSetIndicator.SET)
         sunset!!.timeZone = TimeZone.getTimeZone("GMT")
-        Truth.assertThat(14 <= sunset[Calendar.HOUR_OF_DAY] && 15 >= sunset[Calendar.HOUR_OF_DAY])
-            .isTrue()
-        Truth.assertThat(sunset[Calendar.DAY_OF_MONTH]).isEqualTo(21)
+        assertThat(sunset[Calendar.HOUR_OF_DAY]).isIn(14..15)
+        assertThat(sunset[Calendar.DAY_OF_MONTH]).isEqualTo(21)
         // Sunrise should be between 8:00 and 9:59 the following day.
         sunrise = universe.solarSystemObjectFor(Planet.Sun)
             .calcNextRiseSetTime(testCal, loc, CelestialObject.RiseSetIndicator.RISE)
         sunrise!!.timeZone = TimeZone.getTimeZone("GMT")
-        Truth.assertThat(8 <= sunrise[Calendar.HOUR_OF_DAY] && 9 >= sunrise[Calendar.HOUR_OF_DAY])
-            .isTrue()
-        Truth.assertThat(sunrise[Calendar.DAY_OF_MONTH]).isEqualTo(22)
+        assertThat(sunrise[Calendar.HOUR_OF_DAY]).isIn(8..9)
+        assertThat(sunrise[Calendar.DAY_OF_MONTH]).isEqualTo(22)
 
         // Tests from (60, -75), Northern Quebec, all times EST.
         testCal.timeZone = TimeZone.getTimeZone("EST")
@@ -92,47 +83,41 @@ class PlanetTest {
         sunset = universe.solarSystemObjectFor(Planet.Sun)
             .calcNextRiseSetTime(testCal, loc, CelestialObject.RiseSetIndicator.SET)
         sunset!!.timeZone = TimeZone.getTimeZone("EST")
-        Truth.assertThat(17 <= sunset[Calendar.HOUR_OF_DAY] && 18 >= sunset[Calendar.HOUR_OF_DAY])
-            .isTrue()
-        Truth.assertThat(sunset[Calendar.DAY_OF_MONTH]).isEqualTo(21)
+        assertThat(sunset[Calendar.HOUR_OF_DAY]).isIn(17..18)
+        assertThat(sunset[Calendar.DAY_OF_MONTH]).isEqualTo(21)
         // Sunrise should be between 5:00 and 6:59 the following day.
         sunrise = universe.solarSystemObjectFor(Planet.Sun)
             .calcNextRiseSetTime(testCal, loc, CelestialObject.RiseSetIndicator.RISE)
         sunrise!!.timeZone = TimeZone.getTimeZone("EST")
-        Truth.assertThat(5 <= sunrise[Calendar.HOUR_OF_DAY] && 6 >= sunrise[Calendar.HOUR_OF_DAY])
-            .isTrue()
-        Truth.assertThat(sunrise[Calendar.DAY_OF_MONTH]).isEqualTo(22)
+        assertThat(sunrise[Calendar.HOUR_OF_DAY]).isIn(5..6)
+        assertThat(sunrise[Calendar.DAY_OF_MONTH]).isEqualTo(22)
 
         // In midsummer, sunset should be between 20:00 and 21:59.
         testCal[2010, GregorianCalendar.JUNE, 21, 12, 0] = 0
         sunset = universe.solarSystemObjectFor(Planet.Sun)
             .calcNextRiseSetTime(testCal, loc, CelestialObject.RiseSetIndicator.SET)
         sunset!!.timeZone = TimeZone.getTimeZone("EST")
-        Truth.assertThat(20 <= sunset[Calendar.HOUR_OF_DAY] && 21 >= sunset[Calendar.HOUR_OF_DAY])
-            .isTrue()
-        Truth.assertThat(sunset[Calendar.DAY_OF_MONTH]).isEqualTo(21)
+        assertThat(sunset[Calendar.HOUR_OF_DAY]).isIn(20..21)
+        assertThat(sunset[Calendar.DAY_OF_MONTH]).isEqualTo(21)
         // Sunrise should be between 2:00 and 3:59 the following day.
         sunrise = universe.solarSystemObjectFor(Planet.Sun)
             .calcNextRiseSetTime(testCal, loc, CelestialObject.RiseSetIndicator.RISE)
         sunrise!!.timeZone = TimeZone.getTimeZone("EST")
-        Truth.assertThat(2 <= sunrise[Calendar.HOUR_OF_DAY] && 3 >= sunrise[Calendar.HOUR_OF_DAY])
-            .isTrue()
-        Truth.assertThat(sunrise[Calendar.DAY_OF_MONTH]).isEqualTo(22)
+        assertThat(sunrise[Calendar.HOUR_OF_DAY]).isIn(2..3)
+        assertThat(sunrise[Calendar.DAY_OF_MONTH]).isEqualTo(22)
 
         // In midwinter, sunset should be between 14:00 and 15:59.
         testCal[2010, GregorianCalendar.DECEMBER, 21, 12, 0] = 0
         sunset = universe.solarSystemObjectFor(Planet.Sun)
             .calcNextRiseSetTime(testCal, loc, CelestialObject.RiseSetIndicator.SET)
         sunset!!.timeZone = TimeZone.getTimeZone("EST")
-        Truth.assertThat(14 <= sunset[Calendar.HOUR_OF_DAY] && 15 >= sunset[Calendar.HOUR_OF_DAY])
-            .isTrue()
-        Truth.assertThat(sunset[Calendar.DAY_OF_MONTH]).isEqualTo(21)
+        assertThat(sunset[Calendar.HOUR_OF_DAY]).isIn(14..15)
+        assertThat(sunset[Calendar.DAY_OF_MONTH]).isEqualTo(21)
         // Sunrise should be between 8:00 and 9:59 the following day.
         sunrise = universe.solarSystemObjectFor(Planet.Sun)
             .calcNextRiseSetTime(testCal, loc, CelestialObject.RiseSetIndicator.RISE)
         sunrise!!.timeZone = TimeZone.getTimeZone("EST")
-        Truth.assertThat(8 <= sunrise[Calendar.HOUR_OF_DAY] && 9 >= sunrise[Calendar.HOUR_OF_DAY])
-            .isTrue()
-        Truth.assertThat(sunrise[Calendar.DAY_OF_MONTH]).isEqualTo(22)
+        assertThat(sunrise[Calendar.HOUR_OF_DAY]).isIn(8..9)
+        assertThat(sunrise[Calendar.DAY_OF_MONTH]).isEqualTo(22)
     }
 }
