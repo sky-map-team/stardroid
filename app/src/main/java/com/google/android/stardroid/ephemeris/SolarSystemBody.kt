e7a5b7a0fdc11bf6f2958174d8dabe79b77b1104
jontayler <jontayler@gmail.com>
2021-12-17 22:07:23 -0500

    Whoops I didn't mean to commit all that. Might as well add in these two files: I renamed the Planet enum and its associated rendered as they're not all planets.

---
+++ app/src/main/java/com/google/android/stardroid/ephemeris/SolarSystemBody.kt
@@ -0,0 +1,18 @@
+Unstaged changes after reset:
+M	app/src/main/java/com/google/android/stardroid/activities/dialogs/TimeTravelDialog.java
+D	app/src/main/java/com/google/android/stardroid/ephemeris/Planet.kt
+D	app/src/main/java/com/google/android/stardroid/ephemeris/PlanetRenderable.kt
+M	app/src/main/java/com/google/android/stardroid/ephemeris/SolarSystemBody.kt
+M	app/src/main/java/com/google/android/stardroid/layers/PlanetsLayer.kt
+M	app/src/main/java/com/google/android/stardroid/layers/SkyGradientLayer.kt
+M	app/src/main/java/com/google/android/stardroid/math/Astronomy.kt
+M	app/src/main/java/com/google/android/stardroid/space/CelestialObject.kt
+M	app/src/main/java/com/google/android/stardroid/space/EarthOrbitingObject.kt
+M	app/src/main/java/com/google/android/stardroid/space/Moon.kt
+M	app/src/main/java/com/google/android/stardroid/space/SolarSystemObject.kt
+M	app/src/main/java/com/google/android/stardroid/space/Sun.kt
+M	app/src/main/java/com/google/android/stardroid/space/SunOrbitingObject.kt
+M	app/src/main/java/com/google/android/stardroid/space/Universe.kt
+M	app/src/test/java/com/google/android/stardroid/space/CelestialObjectTest.kt
+M	app/src/test/java/com/google/android/stardroid/space/UniverseSmokeTest.kt
+M	designdocs/ephemeris.md
