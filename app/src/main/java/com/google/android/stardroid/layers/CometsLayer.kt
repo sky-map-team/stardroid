8300d0351a5961686459572cd0f90bbe97f61c6f
jontayler <jontayler@gmail.com>
2021-12-18 21:50:42 -0500

    Work in progress on the Comets Layer.

--- app/src/main/java/com/google/android/stardroid/layers/CometsLayer.kt
+++ app/src/main/java/com/google/android/stardroid/layers/CometsLayer.kt
@@ -14,10 +14,11 @@
 package com.google.android.stardroid.layers
 
 import android.content.res.Resources
-import android.text.format.DateFormat
 import com.google.android.stardroid.R
 import com.google.android.stardroid.base.TimeConstants
 import com.google.android.stardroid.control.AstronomerModel
+import com.google.android.stardroid.math.RaDec.Companion.decDegreesFromDMS
+import com.google.android.stardroid.math.RaDec.Companion.raDegreesFromHMS
 import com.google.android.stardroid.math.Vector3
 import com.google.android.stardroid.math.getGeocentricCoords
 import com.google.android.stardroid.renderables.*
@@ -25,7 +26,7 @@ import com.google.android.stardroid.renderer.RendererObjectManager.UpdateType
 import java.util.*
 import kotlin.math.abs
 
-private val METEOR_SOURCE_PROVIDER = "source_provider.6"
+private const val METEOR_SOURCE_PROVIDER = "source_provider.6"
 
 /**
  * A [Layer] to show the occasional transient comet.
@@ -35,40 +36,66 @@ private val METEOR_SOURCE_PROVIDER = "source_provider.6"
 // Some of this might eventually get generalized for other 'interpolatable' objects.
 class CometsLayer(private val model: AstronomerModel, resources: Resources) :
     AbstractRenderablesLayer(resources, true) {
-    private val showers: MutableList<Shower> = ArrayList()
+    private val comets: MutableList<Comet> = ArrayList()
 
-    /**
-     * Represents a meteor shower.
-     */
-    private class Shower(
-        val nameId: Int, val radiant: Vector3,
-        val start: Date, val peak: Date, val end: Date, val peakMeteorsPerHour: Int
-    )
+    private data class TimeEntry(
+        val date: Date,
+        val raDeg: Float,
+        val decDeg: Float,
+        val mag: Float
+    );
 
-    private fun initializeShowers() {
-        // A list of all the meteor showers with > 10 per hour
-        // Source: http://www.imo.net/calendar/2011#table5
-        // Note the zero-based month. 10=November
-        // Actual start for Quadrantids is December 28 - but we can't cross a year boundary.
-        showers.add(
-            Shower(
-                R.string.earth, getGeocentricCoords(240f, 49f),
-                Date(ANY_OLD_YEAR, 0, 1),
-                Date(ANY_OLD_YEAR, 0, 4),
-                Date(ANY_OLD_YEAR, 11, 31),
-                120
+    private class Interpolator(map: Sequence<Pair<Long, Float>>) {
+
+    }
+
+    private class Comet(
+        val nameId: Int, val positions: List<TimeEntry>
+    ) {
+        val pos = Vector3.zero()
+
+        val start: Date
+        val end: Date
+        var xInterpolator : Interpolator
+        var yInterpolator : Interpolator
+        var zInterpolator : Interpolator
+
+        init {
+            if (positions.isEmpty()) throw IllegalStateException("Comet has no positions")
+            start = positions.first().date
+            end = positions.last().date
+            var previous = start
+            for (entry in positions) {
+                if (entry.date.before(previous)) throw java.lang.IllegalStateException("Comet dates not in ascending order")
+                previous = entry.date
+            }
+            // TODO(johntaylor): come on...are you really going to calculate the geocentric coords 3 times??
+            xInterpolator = Interpolator(positions.asSequence().map {Pair(it.date.time, getGeocentricCoords(it.raDeg, it.decDeg).x)})
+            yInterpolator = Interpolator(positions.asSequence().map {Pair(it.date.time, getGeocentricCoords(it.raDeg, it.decDeg).y)})
+            zInterpolator = Interpolator(positions.asSequence().map {Pair(it.date.time, getGeocentricCoords(it.raDeg, it.decDeg).z)})
+        }
+    }
+
+    private fun initializeComets() {
+        comets.add(
+            Comet(
+                R.string.comet_leonard,
+                listOf(
+                    TimeEntry(Date(2021, 11, 5), raDegreesFromHMS(1f, 2f, 3f), decDegreesFromDMS(1f,2f,3f), 10f),
+                )
             )
         )
 
     }
 
     override fun initializeAstroSources(sources: ArrayList<AstronomicalRenderable>) {
-        for (shower in showers) {
-            sources.add(CometRenderable(model, shower, resources))
+        for (comet in comets) {
+            sources.add(CometRenderable(model, comet, resources))
         }
     }
 
     override val layerDepthOrder = 80
+
     // This is the same as the meteor layer.
     override val preferenceId = METEOR_SOURCE_PROVIDER
     override val layerName = "Comets"
@@ -76,45 +103,30 @@ class CometsLayer(private val model: AstronomerModel, resources: Resources) :
 
     private class CometRenderable(
         private val model: AstronomerModel,
-        private val shower: Shower,
+        private val comet: Comet,
         resources: Resources
     ) : AbstractAstronomicalRenderable() {
-        override val labels: MutableList<TextPrimitive> = ArrayList()
-        override val images: MutableList<ImagePrimitive> = ArrayList()
+        // TODO(johntaylor): why are we overriding these properties?
+        override val labels = ArrayList<TextPrimitive>()
+        override val images = ArrayList<ImagePrimitive>()
         private var lastUpdateTimeMs = 0L
         private val theImage: ImagePrimitive
         private val label: TextPrimitive
-        private val name = resources.getString(shower.nameId)
-        override val names: MutableList<String> = ArrayList()
+        private val name = resources.getString(comet.nameId)
+        override val names = ArrayList<String>()
         override val searchLocation: Vector3
-            get() = shower.radiant
+            get() = comet.pos
 
         private fun updateShower() {
             lastUpdateTimeMs = model.time.time
-            // We will only show the shower if it's the right time of year.
+            // We will only show the comet between certain times.
             val now = model.time
-            // Standardize on the same year as we stored for the showers.
-            now.year = ANY_OLD_YEAR
             theImage.setUpVector(UP)
-            // TODO(johntaylor): consider varying the sizes by scaling factor as time progresses.
-            if (now.after(shower.start) && now.before(shower.end)) {
+            if (now.after(comet.start) && now.before(comet.end)) {
                 label.text = name
-                val percentToPeak = if (now.before(shower.peak)) {
-                    (now.time - shower.start.time).toDouble() /
-                            (shower.peak.time - shower.start.time)
-                } else {
-                    (shower.end.time - now.time).toDouble() /
-                            (shower.end.time - shower.peak.time)
-                }
-                // Not sure how best to calculate number of meteors - use linear interpolation for now.
-                val numberOfMeteorsPerHour = shower.peakMeteorsPerHour * percentToPeak
-                if (numberOfMeteorsPerHour > METEOR_THRESHOLD_PER_HR) {
-                    theImage.setImageId(R.drawable.meteor2_screen)
-                } else {
-                    theImage.setImageId(R.drawable.meteor1_screen)
-                }
                 theImage.setImageId(R.drawable.earth) // temp placeholder!
             } else {
+                // TODO(johntaylor): we need a better solution than just blanking out the object!
                 label.text = " "
                 theImage.setImageId(R.drawable.blank)
             }
@@ -142,32 +154,20 @@ class CometsLayer(private val model: AstronomerModel, resources: Resources) :
         }
 
         init {
-            // Not sure what the right user experience should be here.  Should we only show up
-            // in the search results when the shower is visible?  For now, just ensure
-            // that it's obvious from the search label.
-            val startDate = DateFormat.format("MMM dd", shower.start)
-            val endDate = DateFormat.format("MMM dd", shower.end)
-            names.add("$name ($startDate-$endDate)")
+            names.add(name)
             // blank is a 1pxX1px image that should be invisible.
             // We'd prefer not to show any image except on the shower dates, but there
             // appears to be a bug in the renderer/layer interface in that Update values are not
             // respected.  Ditto the label.
             // TODO(johntaylor): fix the bug and remove this blank image
-            theImage = ImagePrimitive(shower.radiant, resources, R.drawable.blank, UP, SCALE_FACTOR)
+            theImage = ImagePrimitive(comet.pos, resources, R.drawable.blank, UP, SCALE_FACTOR)
             images.add(theImage)
-            label = TextPrimitive(shower.radiant, name, LABEL_COLOR)
+            label = TextPrimitive(comet.pos, name, LABEL_COLOR)
             labels.add(label)
         }
     }
 
-    companion object {
-        private const val ANY_OLD_YEAR = 100 // = year 2000
-
-        /** Number of meteors per hour for the larger graphic  */
-        private const val METEOR_THRESHOLD_PER_HR = 10.0
-    }
-
     init {
-        initializeShowers()
+        initializeComets()
     }
 }
