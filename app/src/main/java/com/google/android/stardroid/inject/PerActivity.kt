ec4f9c57a121c00f1210d9df69c30e91c7b45c11
jontayler <jontayler@gmail.com>
2021-12-24 19:06:15 -0500

    More auto-Kotlin conversion.

---
+++ app/src/main/java/com/google/android/stardroid/inject/PerActivity.kt
@@ -0,0 +1,9 @@
+package com.google.android.stardroid.inject
+
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import javax.inject.Scope
+
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+annotation class PerActivity 
