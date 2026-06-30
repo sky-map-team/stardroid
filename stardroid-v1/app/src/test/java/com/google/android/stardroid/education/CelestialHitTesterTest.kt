54cba616b6970ac72985c126c0c6662d6c2d83be
JayDeeTay <skymapdevs@gmail.com>
2026-06-30 19:49:45 -0400

    Resolve contradictory licenses in files with existing Apache 2.0 boilerplates

--- stardroid-v1/app/src/test/java/com/google/android/stardroid/education/CelestialHitTesterTest.kt
+++ stardroid-v1/app/src/test/java/com/google/android/stardroid/education/CelestialHitTesterTest.kt
@@ -1,23 +1,19 @@
 /*
+ * Copyright (C) 2011 Google Inc.
  * Copyright (c) 2026 Penterakt LLC. All rights reserved.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 package com.google.android.stardroid.education
 
 import com.google.android.stardroid.control.AstronomerModel
